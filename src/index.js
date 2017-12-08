0b6eb569a752cf0452ac201d31837adf47f213f9
Yibin Lin <lyyb46@gmail.com>
2017-12-08 15:00:29 -0800

    lecture 29, passing in call backs two levels

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VidieoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyAVCcNNt9lLXcW5ruxNO_UClVAeSvi9Bx0';
 
@@ -11,10 +12,16 @@ class App extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { videos: [] };
+        this.state = { 
+            videos: [],
+            selectedVideo: null
+        };
 
         YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-            this.setState({ videos });
+            this.setState({ 
+                videos: videos,
+                selectedVideo: videos[0]
+            });
         })
     }
 
@@ -22,7 +29,10 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
-                <VideoList videos={this.state.videos} />
+                <VidieoDetail video={this.state.selectedVideo}/>
+                <VideoList 
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos} />
             </div>
         )
     }
