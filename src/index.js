02cc33f809389f5fbf7224b44c5306c0b496b466
Yibin Lin <lyyb46@gmail.com>
2017-12-12 14:01:31 -0800

    lecture 33

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -30,16 +31,18 @@ class App extends Component {
     }
 
     render() {
+        const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
         return (
             <div>
                 <SearchBar 
-                    onSearchTermChange={term => this.videoSearch(term)} />
+                    onSearchTermChange={videoSearch} />
                 <VidieoDetail video={this.state.selectedVideo}/>
                 <VideoList 
                     onVideoSelect={selectedVideo => this.setState({selectedVideo})}
                     videos={this.state.videos} />
             </div>
-        )
+        );
     }
 }
 
