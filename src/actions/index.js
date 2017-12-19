733734a410126e014c3ae6004969a18341718722
Yibin Lin <lyyb46@gmail.com>
2017-12-19 09:56:13 -0800

    lecture 48

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 export function selectBook(book) {
     // selectBook is an ActionCreator, it needs to return an action,
-    // an object with type property.
+1    // an object with type property.
     return {
         type: 'BOOK_SELECTED',
         payload: book
