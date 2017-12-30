f5e9083086e0ed33d4a57243269f7f70cb03ebfa
Yibin Lin <lyyb46@gmail.com>
2017-12-29 18:44:55 -0800

    middleware, lecture 57

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,17 @@
-API_KEY='e0cd0b0d28eec5113bc2c16129f66e65';
+import axios from 'axios';
 
+const API_KEY='e0cd0b0d28eec5113bc2c16129f66e65';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+// For compile-time check, create a variable.
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
