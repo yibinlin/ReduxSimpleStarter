35f20f568094de367a479142ff3194b438f9596f
Yibin Lin <lyyb46@gmail.com>
2017-12-29 21:43:09 -0800

    lecture 62

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,5 +1,12 @@
-export default function(state = null, action) {
-    console.log('Action received', action);
+import { FETCH_WEATHER } from '../actions/index';
 
+export default function(state = [], action) {
+    switch (action.type) {
+        case FETCH_WEATHER:
+            // returning a completely new state instance
+            // return state.concat([action.payload.data]);
+            // ES6 syntax equivalent
+            return [ action.payload.data, ...state ];
+    }
     return state;
 }
