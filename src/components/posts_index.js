2a4d26a337e1656e3b8518d460cd3c58522ce9c1
Yibin Lin <lyyb46@gmail.com>
2018-01-11 22:55:19 -0800

    lecture 123

---
+++ src/components/posts_index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class PostIndex extends Component {
+    render() {
+        return (
+            <div>
+                Posts Index
+            </div>
+        );
+    }
+}
