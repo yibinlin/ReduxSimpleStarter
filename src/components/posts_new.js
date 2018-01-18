3ec789b3c7169df14b0dcd6a04e557bc86f853c7
Yibin Lin <lyyb46@gmail.com>
2018-01-18 11:08:19 -0800

    lecture 133

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
+// reduxForm is a function like connect helper, talks to formReducer.
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
     render() {
         return (
-            <div>
-                PostsNew!
-            </div>
+            <form>
+                <Field 
+                    name="title"
+                    component={}
+                />
+            </form>
         );
     }
 }
 
-export default PostsNew;
+export default reduxForm({
+    // name of the form.
+    form: 'PostsNewForm'
+})(PostsNew);
