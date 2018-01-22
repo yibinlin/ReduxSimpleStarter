d47b99eb957637d6c270d4d21db7be5c5efa90eb
Yibin Lin <lyyb46@gmail.com>
2018-01-22 10:33:54 -0800

    lecture 145

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,8 +1,15 @@
 import _ from 'lodash';
-import { FETCH_POSTS } from '../actions';
+import { FETCH_POSTS, FETCH_POST } from '../actions';
 
 export default function(state = {}, action) {
     switch (action.type) {
+        case FETCH_POST:
+            // const post = action.payload.data;
+            // const newState = { ...state };
+            // newState[post.id] = post;
+
+            // new ES6 syntax
+            return { ...state, [action.payload.data.id]: action.payload.data };
         case FETCH_POSTS:
             // [post1, post2] => { 1:post, 2:post }
             return _.mapKeys(action.payload.data, 'id');
