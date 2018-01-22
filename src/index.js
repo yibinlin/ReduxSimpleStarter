c8dbd003d207927d0ba491ebe5346193e01369a7
Yibin Lin <lyyb46@gmail.com>
2018-01-22 10:08:55 -0800

    lecture 145

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import promise from 'redux-promise';
 import reducers from './reducers';
 import PostsIndex from './components/posts_index';
 import PostsNew from './components/posts_new';
+import PostsShow from './components/posts_show';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
@@ -17,6 +18,8 @@ ReactDOM.render(
         <div>
           <Switch>
             <Route path="/posts/new" component={PostsNew} />
+            {/* will pass whatevery is after /posts/ and put into props.id. */}
+            <Route path="/posts/:id" component={PostsShow} />
             <Route path="/" component={PostsIndex} />
           </Switch>
         </div>
