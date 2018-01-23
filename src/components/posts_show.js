2b7ad6d26e3f1d14cc8faeff38a524d927881a58
Yibin Lin <lyyb46@gmail.com>
2018-01-23 10:40:49 -0800

    lecture 14

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions';
 
 class PostsShow extends Component {
+    componentDidMount() {
+        // provided by router.
+        const id = this.props.match.params.id;
+        this.props.fetchPost(id);
+    }
+
     render() {
+        posts[]
         return (
             <div>
                 Posts Show!
@@ -10,4 +19,8 @@ class PostsShow extends Component {
     }
 }
 
-export default PostsShow;
+function mapStateToProps({ posts }, ownProps) {
+    return { post: posts[ownProps.match.params.id] };
+}
+
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
